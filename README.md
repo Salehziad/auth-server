a0de5dcee1313c8b7a305fc53d945fb69d72afd6
Salehziad <salehziad1999@gmail.com>
2022-06-16 08:26:53 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # auth-server
+## server features:
+> - `/` (`get ,ethod`): to show home page (`localhost:3000/`).
+> - `/signup` (`post method`) : to enter a new user with username and password (`localhost:3000/signup`).
+> - `/signin` (`post method`) : to log in with samse name and password and show user data  (`localhost:3000/signin`).
+> - `/users` (`get method`) : to show user data by the tokin from login (`localhost:3000/users`).
+> - `/secret` (`get method`) : to go to the secrt area by the tokin (`localhost:3000/secret`).
+## How to run server:
+>### 1- by run in local:
+>> - clone this repo to your local machine.
+>> - cd to the repo.
+>> - in terminal run `npm install` to install backage that used in this server `express , dotenv , jest , supertest , nodemon`
+>> - after that run npm `index.js` ore use `nodemon`.
+>> - there is multible way to see result of this server by browser or thunder client . 
+>> - if u want to add feature to this server there is `test` by `jest` you can test features with it before add to main.
+>> - if you see any issue with my server fork my repo with datals and solution if you have it.
+>### 2- by use online app for this server by heroku 
+>> - [saleh-basic-api-server](https://saleh-bearer-auth.herokuapp.com/)
+
+
+## About this server
+> - this server untel now undr workinf and many features will be added soon.
+> - the food and clothes method make in this server as glopal middle war same as logger.
+> - this server contains `middleWar` way for write functions.
+> - this server contain  `404 error , 500 eror` handler. 
+> - this server has test for all methods you can test before use anytime :).\
+
+# heroku link
+> [saleh-basic-api-server](https://saleh-bearer-auth.herokuapp.com/)
+
+# GitHub actions link
+> - [actions link](https://github.com/Salehziad/bearer-auth/actionsa)
+
+# The pull request link
+> - [pull request](https://github.com/Salehziad/bearer-auth/pulls?q=is%3Apr+is%3Aclosed)
--- __test__/middleware/basic-auth-middleware.test.js
+++ __test__/middleware/basic-auth-middleware.test.js
@@ -1,68 +1,58 @@
 'use strict';
-
 process.env.SECRET = "TEST_SECRET";
-
 const base64 = require('base-64');
 const middleware = require('../../src/auth/middleware/basic.js');
-const { db, users } = require('../../src/auth/models/index.js');
-
+const {
+    db,
+    users
+} = require('../../src/auth/models/index.js');
 let userInfo = {
-  admin: { username: 'admin-basic', password: 'password' },
+    admin: {
+        username: 'admin-basic',
+        password: 'password'
+    },
 };
-console.log('ssssssssssssssssssssssss',users);
-// Pre-load our database with fake users
+console.log('ssssssssssssssssssssssss', users);
 beforeAll(async () => {
-  await db.sync();
-  await users.create(userInfo.admin);
+    await db.sync();
+    await users.create(userInfo.admin);
 });
 afterAll(async () => {
-  await db.drop();
+    await db.drop();
 });
-
 describe('Auth Middleware', () => {
-
-  // admin:password: YWRtaW46cGFzc3dvcmQ=
-  // admin:foo: YWRtaW46Zm9v
-
-  // Mock the express req/res/next that we need for each middleware call
-  const req = {};
-  const res = {
-    status: jest.fn(() => res),
-    send: jest.fn(() => res)
-  }
-  const next = jest.fn();
-
-  describe('user authentication', () => {
-
-    it('fails a login for a user (admin) with the incorrect basic credentials', () => {
-      const basicAuthString = base64.encode('username:password');
-
-      // Change the request to match this test case
-      req.headers = {
-        authorization: `Basic ${basicAuthString}`,
-      };
-
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).not.toHaveBeenCalled();
-          expect(res.status).toHaveBeenCalledWith(403);
+    // Mock the express req/res/next that we need for each middleware call
+    const req = {};
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(() => res)
+    }
+    const next = jest.fn();
+    describe('user authentication', () => {
+        it('fails a login for a user (admin) with the incorrect basic credentials', () => {
+            const basicAuthString = base64.encode('username:password');
+            // Change the request to match this test case
+            req.headers = {
+                authorization: `Basic ${basicAuthString}`,
+            };
+            return middleware(req, res, next)
+                .then(() => {
+                    expect(next).not.toHaveBeenCalled();
+                    expect(res.status).toHaveBeenCalledWith(403);
+                });
         });
 
-    });
+        it('logs in an admin user with the right credentials', () => {
+            let basicAuthString = base64.encode(`${userInfo.admin.username}:${userInfo.admin.password}`);
+            // Change the request to match this test case
+            req.headers = {
+                authorization: `Basic ${basicAuthString}`,
+            };
+            return middleware(req, res, next)
+                .then(() => {
+                    expect(next).not.toHaveBeenCalledWith();
+                });
 
-    it('logs in an admin user with the right credentials', () => {
-      let basicAuthString = base64.encode(`${userInfo.admin.username}:${userInfo.admin.password}`);
-
-      // Change the request to match this test case
-      req.headers = {
-        authorization: `Basic ${basicAuthString}`,
-      };
-
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).not.toHaveBeenCalledWith();
         });
-
     });
-  });
 });
